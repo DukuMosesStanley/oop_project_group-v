fa1d4d44ff4b8286138b4d703819ddd3593ee9cf
DukuMosesStanley <47744542+DukuMosesStanley@users.noreply.github.com>
2019-09-23 14:01:11 +0300

    Add files via upload

---
+++ oop_project_group v/src/oop_project_group/model/Role.java
@@ -0,0 +1,20 @@
+
+package oop_project_group.model;
+
+/*
+        Group members
+        =============
+        
+        Name                                 Registration Number
+        ====                                 ===================
+        CHANCE LUWONG                        18/847/BIT-S
+        DUKU MOSES                           18/927/BSSE-S
+        RICHARD KAMULAK JUSTIN LOKOSANG      18/1176/BIT-S
+        PHILIP JURUGO                         18/1068/BIT-S
+
+        */
+
+public class Role {
+    
+}
+        
